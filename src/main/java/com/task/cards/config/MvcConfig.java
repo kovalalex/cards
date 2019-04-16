1b57a3c216a7ffc6672d2cd9e02a936dc1539184
kovalalex <krevedk-o@ya.ru>
2019-04-17 00:53:12 +0500

    Corrected upload.path property usage

--- src/main/java/com/task/cards/config/MvcConfig.java
+++ src/main/java/com/task/cards/config/MvcConfig.java
@@ -3,7 +3,6 @@ package com.task.cards.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,7 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/xml/**")
-                .addResourceLocations("file:///" + uploadPath + "/");
+                .addResourceLocations("file://" + uploadPath + "/");
         registry.addResourceHandler("/static/*")
                 .addResourceLocations("classpath:/static/");
 
